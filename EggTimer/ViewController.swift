e1e4c793837e01036adcf9621b652fb85034ce2c
Janibek <jano.vr@mail.ru>
2023-01-26 14:31:57 +0400

    Done Application + Alarm

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -6,29 +6,52 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
-    let eggTimes = ["Soft": 300,
-                    "Medium": 420,
-                    "Hard": 720]
-    var secondsRemaining = 60
-
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    var player: AVAudioPlayer!
+    
+    let eggTimes = ["Soft": 3,
+                    "Medium": 4,
+                    "Hard": 7]
+    
+    var totalTime: Float = 0
+    var secondsPassed: Float = 0
+    
+    var timer = Timer()
+    
     @IBAction func hardnessSelected(_ sender: UIButton) {
+         
         
+        timer.invalidate()
         let hardness = sender.titleLabel!.text! //Soft, Medium, Hard
+        totalTime = Float(eggTimes[hardness]!)
         
-        secondsRemaining = eggTimes[hardness]!
+        progressView.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
         
-        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
-        
-        @objc func updateTimer() {
-            if secondsRemaining > 0 {
-                print("\(secondsRemaining) seconds.")
-                secondsRemaining -= 1
-            }
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        }
+    
+    @objc func updateTimer() {
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            progressView.progress = secondsPassed / totalTime
+
+        } else {
+            timer.invalidate()
+            titleLabel.text = "DONE!"
+            
+            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
+            
         }
     }
-     
 }
 
